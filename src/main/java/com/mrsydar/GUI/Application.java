6511e6fe13fce7eeab73983bf38b11cdee573184
MrSydar <dengrom1@gmail.com>
2020-10-08 13:11:00 +0300

    First launch

---
+++ src/main/java/com/mrsydar/GUI/Application.java
@@ -0,0 +1,16 @@
+package com.mrsydar.GUI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Application extends JFrame {
+
+    Container container = getContentPane();
+    JLabel userGmailLabel = new JLabel("GMAIL");
+
+    public Application() {
+
+    }
+}
---
+++ src/main/java/com/mrsydar/Main.java
@@ -0,0 +1,11 @@
+package com.mrsydar;
+
+import com.mrsydar.GUI.Application;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Application app = new Application();
+    }
+
+}
